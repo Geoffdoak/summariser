04110bb499f9fd1ecb22deb8874b139df5a0dabf
Geoffrey Doak <gdoak@pop-os.localdomain>
2023-08-29 23:08:10 +1000

    added homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { AnimationWrapper } from "@/components/animationWrapper"
 import { LoginButton, LogoutButton } from "@/components/auth"
 import { prisma } from "@/lib/prisma"
 import { getServerSession } from "next-auth"
+import Link from "next/link"
 import { authOptions } from "./api/auth/[...nextauth]/route"
 // import { TestSummarise } from "@/openai"
 
@@ -19,30 +20,14 @@ export default async function Home() {
 
   return (
     <AnimationWrapper>
-      <main>
-        <LoginButton></LoginButton>
-        <LogoutButton></LogoutButton>
-        {user?.name}
-        {JSON.stringify(session)}
-        <ul>
-          {/* {response.map((response, index) => {
-            return (
-              <li key={index}>
-                <h2>
-                  {response.questionContent}
-                </h2>
-                <div>Ask count: {response.askedQuestions.length + 1}</div>
-                <ul className="ml-5">
-                  {response.askedQuestions.map((q, index) => {
-                    return (
-                      <li key={index}>{q}</li>
-                    )
-                  })}
-                </ul>
-              </li>
-            )
-          })} */}
-        </ul>
+      <main className="flex flex-col items-center">
+        <h1 className="text-5xl font-bold mb-5">Summariser</h1>
+        <p className="max-w-xl text-2xl text-center mb-5">
+          Send out a form where people can ask questions, and Summariser will filter out the unique questions and rank them.
+        </p>
+        <div className="text-2xl">
+          <Link href="/register" className="text-sky-700">Sign up</Link> or <Link href="/login" className="text-sky-700">Sign In</Link>
+        </div>
       </main>
     </AnimationWrapper>
   )
