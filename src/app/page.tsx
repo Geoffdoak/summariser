fc9e77a890f16d88ea3cde5680a49bd36ab0e274
Geoffrey Doak <gdoak@pop-os.localdomain>
2023-08-29 23:10:47 +1000

    removed unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,8 @@
 import { AnimationWrapper } from "@/components/animationWrapper"
-import { LoginButton, LogoutButton } from "@/components/auth"
-import { prisma } from "@/lib/prisma"
-import { getServerSession } from "next-auth"
 import Link from "next/link"
-import { authOptions } from "./api/auth/[...nextauth]/route"
-// import { TestSummarise } from "@/openai"
 
 
 export default async function Home() {
-  const user = await prisma.user.findFirst({
-    where: {
-      email: 'test@test.com'
-    }
-  })
-
-  const session = await getServerSession(authOptions)
-
-  // const response = await TestSummarise()
-
   return (
     <AnimationWrapper>
       <main className="flex flex-col items-center">
