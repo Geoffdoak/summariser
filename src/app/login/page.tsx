f6e48a95944dfe1fbf508ee7d4b921cf1b66a982
Geoffrey Doak <gdoak@pop-os.localdomain>
2023-09-04 16:28:05 +1000

    added validation to login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,12 +2,28 @@
 
 import { signIn } from "next-auth/react"
 import { Button, Card, CardBody, CardHeader, Input } from '@nextui-org/react'
-import { useState } from "react"
 import { AnimationWrapper } from "@/components/animationWrapper"
+import useValidInput, { ValidatorType } from '@/hooks/forms/useValidInput'
+
+const emailValidator: ValidatorType = function (input: string) {
+  const regex = new RegExp(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i)
+  return {
+    isValid: regex.test(input),
+    message: 'Must be valid email'
+  }
+}
+
+const passwordValidator: ValidatorType = function(input: string) {
+  return {
+    isValid: input.length > 5,
+    message: 'Must be at least six characters'
+  }
+}
 
 export default function SignIn() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const email = useValidInput('', emailValidator)
+  const password = useValidInput('', passwordValidator)
+  const allInputsValid = email.isValid && password.isValid
 
   const onSubmit = async function () {
     const url = window.origin + '/dashboard'
@@ -24,24 +40,32 @@ export default function SignIn() {
               <Input
                 label={'Email'}
                 required
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                value={email.content}
+                onChange={(e) => email.set(e.target.value)}
                 id="email"
                 type="email"
+                validationState={email.showError ? 'invalid' : 'valid'}
+                errorMessage={email.message}
               />
             </div>
             <div className="mb-5">
               <Input
                 label={'Password'}
                 required
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                value={password.content}
+                onChange={(e) => password.set(e.target.value)}
                 id="password"
                 type="password"
+                validationState={password.showError ? 'invalid' : 'valid'}
+                errorMessage={password.message}
               />
             </div>
             <div className="w-full">
-              <Button className="w-full" onPress={onSubmit}>
+              <Button
+                className="w-full"
+                onPress={onSubmit}
+                isDisabled={!allInputsValid}
+              >
                 Sign In
               </Button>
             </div>
